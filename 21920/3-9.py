c567e0c16c7da3422355386e670551b44b19de16
6894556 <6894556@gmail.ocm>
2022-08-01 23:40:40 +0900

    rename 21920 files

--- 21920/3-9.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtCore import QDate, QTime, QDateTime
-
-now = QDate.currentDate()
-time = QTime.currentTime()
-datetime = QDateTime.currentDateTime()
-print(now.toString())
-print(time.toString())
-print(datetime.toString())
